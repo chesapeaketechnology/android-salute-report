5b1ef62eb197538983e8dd65fc84b9c8e893cb34
christian.rowlands <crowlands@ctic-inc.com>
2020-04-02 15:13:58 -0400

    MONKEY-55: Updated the Report Details Layout to use a linear layout
    - Updated some of the logic to handle back navigation
    - Removed the toast that is displayed when a salute report loading fails
    - Synchronized the calls for loading and creating Salute Reports

--- app/src/main/java/com/chesapeaketechnology/salute/MainActivity.java
+++ app/src/main/java/com/chesapeaketechnology/salute/MainActivity.java
@@ -1,17 +1,20 @@
 package com.chesapeaketechnology.salute;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+/**
+ * The main activity for the Salute Report app.  The Salute Report app allows for users to create SALUTE Reports that
+ * are saved to a JSON file and then shared to Sync Monkey.
+ *
+ * @since 0.1.0
+ */
 public class MainActivity extends AppCompatActivity
 {
     private AppBarConfiguration appBarConfiguration;
@@ -34,15 +37,6 @@ public class MainActivity extends AppCompatActivity
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
-    /* Delete me
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }*/
-
     /**
      * Configure navigation so toolbar back button works properly.
      *
@@ -54,43 +48,5 @@ public class MainActivity extends AppCompatActivity
         appBarConfiguration = new AppBarConfiguration.Builder(R.id.HomeFragment).build();
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        OnBackPressedCallback callback = new OnBackPressedCallback(true)
-        {
-            @Override
-            public void handleOnBackPressed()
-            {
-                // For whatever reason calling navigateUp from one of the top level destinations results in the
-                // navigation drawer being opened.  Therefore, if the current destination a top level we have custom
-                // code here to move this activity to the back stack.
-                final NavDestination currentDestination = navController.getCurrentDestination();
-                if (currentDestination != null && currentDestination.getId() == R.id.home)
-                {
-                    moveTaskToBack(true);
-                } else
-                {
-                    NavigationUI.navigateUp(navController, appBarConfiguration);
-                }
-            }
-        };
-        getOnBackPressedDispatcher().addCallback(this, callback);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == android.R.id.home)
-        {
-            onBackPressed();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
